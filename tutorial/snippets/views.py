8501897db6e3f43b1790e47deac2460e65d9d774
Giorgi1971 <giooorgi7@gmail.com>
2021-03-07 21:49:38 +0400

    hold on this some time

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,8 +6,18 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
